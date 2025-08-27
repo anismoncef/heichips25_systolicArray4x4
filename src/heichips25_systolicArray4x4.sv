c262935a876ba36041ed58df8fe727b3a4be7ddc
aniskeysom <ayagoub@keysom.io>
2025-08-27 20:50:18 +0200

    fix(name): fixed name
    Signed-off-by: aniskeysom <ayagoub@keysom.io>

--- src/heichips25_systolicArray4x4.sv
+++ src/heichips25_systolicArray4x4.sv
@@ -5,7 +5,7 @@
 `include "./heichips25_systolicArray.sv"
 `default_nettype none
 
-module heichips25_template (
+module heichips25_systolicArray4x4 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
